c237fe269dcd13224d8fda4ac1530e4b7412997a
Ajeesh B Nair <ajeeshklr@Ajeeshs-MacBook-Pro.local>
2018-03-23 19:39:21 +0530

    Initial commit

---
+++ Advanced/advanced.js
@@ -0,0 +1,10 @@
+const _nameConst = "Name";
+const _ageConst = "Age";
+
+let person = function (){
+  let _name = "";
+  let _age = "";
+   function set[_nameConst] (value){
+     _name = value;
+   }
+}
