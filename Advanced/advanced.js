f96dbe1e5b3cecab41b1feb3ccd36adf881c1c1e
ajeeshklr <ajeeshklr@gmail.com>
2018-03-23 20:07:36 +0530

    compilation issues addressed

--- Advanced/advanced.js
+++ Advanced/advanced.js
@@ -1,10 +1,34 @@
-const _nameConst = "Name";
-const _ageConst = "Age";
+const __firstNameConst = "FirstName";
+const __middleNameConst = "MiddleName";
+const __lastNameConst = "LastName";
 
-let person = function (){
-  let _name = "";
-  let _age = "";
-   function set[_nameConst] (value){
-     _name = value;
-   }
+const __ageConst = "Age";
+
+class Person{
+
+ set [__firstNameConst] (value){
+   this._firstName = value;
+ }
+
+ get [__firstNameConst] (){
+   return this._firstName;
+ }
+
+ set [__lastNameConst] (value){
+   this._lastName = value;
+ }
+
+ get [__lastNameConst] () { return this._lastName ; }
+
+ set [__middleNameConst] (value ) { this._middleName = value  }
+
+ get [__middleNameConst] () { return this._middleName; }
+
+ set [__ageConst] (value) { this._age = value; }
+
+ get [__ageConst] () { return this._age; }
+ 
+
+
+
 }
